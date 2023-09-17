7aa91dc7447876bff12fea55ab48ae19abf521b4
jorpelu <jorgeperez96jpl@gmail.com>
2023-09-17 19:57:34 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Sidebar/>
+  <RouterView></RouterView>
+</template>
+
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Sidebar from './components/sidebar.vue';
+
+</script>
+
+
+
